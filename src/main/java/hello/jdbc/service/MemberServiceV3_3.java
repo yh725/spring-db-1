fd50d625cb3b6add13f42afcd181ae0d08c5d6f8
yh725 <yh725@kakao.com>
2023-04-18 18:29:32 +0900

    스프링과 문제 해결(트랜잭션) - 트랜잭션 문제 해결(트랜잭션 AOP 이해) - 트랜잭션 문제 해결(트랜잭션 AOP 적용) - 트랜잭션 문제 해결(트랜잭션 AOP 정리) - 스프링 부트의 자동 리소스 등록

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -2,38 +2,28 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
-@RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_3 {
 
-	//	private final DataSource dataSource;
-	private final PlatformTransactionManager transactionManager;
 	private final MemberRepositoryV3 memberRepository;
 
-	public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-//		Connection con = dataSource.getConnection();
-		//트랜잭션 시작
-		TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+	public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
-		try {
-			bizLogic(fromId, toId, money);
-			transactionManager.commit(status); //성공시 커밋
-		} catch (Exception e) {
-			transactionManager.rollback(status); //실패시 롤백
-			throw new IllegalStateException(e);
-		}
+	@Transactional
+	public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+		bizLogic(fromId, toId, money);
 	}
 
 	private void bizLogic(String fromId, String toId, int money) throws SQLException {
@@ -51,4 +41,5 @@ public class MemberServiceV3_1 {
 			throw new IllegalStateException("이체중 예외 발생");
 		}
 	}
+
 }
